35cc1297ba97c5a4a820e0f1a7c712f25fcb4801
Geeth S Gunnampalli <geeth.gunnampalli@gmail.com>
2022-06-28 07:45:09 +0530

    Add Color Mode Support

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript, theme } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+
       <App />
     </ChakraProvider>
   </React.StrictMode>
